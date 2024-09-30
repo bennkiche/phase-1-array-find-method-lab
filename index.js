36c55b052b87178b89d36105945f08e03ab79717
Benn <bennkiche7@gmail.com>
2024-09-30 22:58:14 +0300

    completetask

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+  
+
+  const winningYear = superbowlWin(record);
+  console.log(winningYear);
